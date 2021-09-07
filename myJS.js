427dabcffc35825d9cf01a8296881fa791acc9ff
Bhaskar Mahor <bhaskarmahor1@gmail.com>
2021-09-07 14:22:03 +0530

    update

---
+++ myJS.js
@@ -0,0 +1,9 @@
+var player1 = prompt("Player One: Enter your name, you will be Blue");
+var player1Color = 'rgb(86, 151, 255)';
+
+var player2 = prompt("Player Two: Enter your name, you will be Red");
+var player2Color = 'rgb(237, 45, 73)';
+
+var game_on = true;
+var table = $('table tr');
+
